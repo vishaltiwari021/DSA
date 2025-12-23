0773d5ee0671d58a9c42ff4f8c46f4070100b6d2
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2025-12-23 17:48:52 +0530

    update the code

--- src/com/Arrays/ReverseOfArray.java
+++ src/com/Arrays/ReverseOfArray.java
@@ -1,7 +1,29 @@
 package com.Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReverseOfArray {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Reverse Array:-");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int left  = 0 ;
+        int right = arr.length-1;
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.print(Arrays.toString(arr));
+
+
     }
 }
